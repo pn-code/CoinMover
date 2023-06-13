289994e9354d09910c68eaf0d1c266cfa1eb71c7
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-12 22:47:23 -0700

    Updated & deployed smart contract to test network

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,9 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: "",
+      accounts: []
+    }
+  }
 };
 
 export default config;
