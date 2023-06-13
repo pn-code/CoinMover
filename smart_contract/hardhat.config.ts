9158c75603b5f73c9cc985702ede55c1d01270c4
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-13 14:07:43 -0700

    Removed config import which causes error on deploy

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,8 +1,7 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 
-const config: HardhatUserConfig = {
+const config = {
   solidity: "0.8.18",
   networks: {
     sepolia: {
